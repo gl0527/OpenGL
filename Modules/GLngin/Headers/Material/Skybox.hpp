fa54094a2ebe3d9c6b1d216e167c56f6287b31d0
gl0527 <gurzolajos27@gmail.com>
2020-05-20 21:59:07 +0200

    Add Skybox material

---
+++ Modules/GLngin/Headers/Material/Skybox.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef GLNGIN_MATERIAL_SKYBOX_HPP
+#define GLNGIN_MATERIAL_SKYBOX_HPP
+
+#include "API.hpp"
+#include "Material.hpp"
+
+namespace GLngin {
+namespace Material {
+
+class Skybox : public Material {
+public:
+    Skybox(const std::string (&_names)[6]);
+
+    virtual void Bind() override;
+    virtual void Bind(const PerObjectData &pod, const PerFrameData &pfd) override;
+
+private:
+    std::string names[6];
+};
+
+}  // namespace Material
+}  // namespace GLngin
+
+#endif  // #ifndef GLNGIN_MATERIAL_SKYBOX_HPP
---
+++ Modules/GLngin/Sources/Material/Skybox.cpp
@@ -0,0 +1,31 @@
+#include "Material/Skybox.hpp"
+
+#include "ShaderSamples.hpp"
+#include "TextureCube.hpp"
+#include "RenderState.hpp"
+
+namespace GLngin {
+namespace Material {
+
+Skybox::Skybox(const std::string (&_names)[6])
+{
+    shader.Init(BackgroundVS, std::nullopt, std::nullopt, std::nullopt, BackgroundFS, std::nullopt);
+    for (unsigned char i = 0; i < 6; ++i) {
+        names[i] = _names[i];
+    }
+}
+
+void Skybox::Bind()
+{
+    shader.Bind();
+    shader.SetUniformTextureCube("skybox", GLngin::TextureCube::GetID(names), 0);
+}
+
+void Skybox::Bind(const PerObjectData &pod, const PerFrameData &pfd)
+{
+    shader.Bind();
+    shader.SetUniformMat4("MVP", pod.MVP);
+}
+
+}  // namespace Material
+}  // namespace GLngin
